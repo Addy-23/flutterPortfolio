aa3f4c07e344070603aa710ca2d4044265fd7bfd
Aditya Bhapkar <23121999addy@gmail.com>
2022-01-29 22:20:05 +0530

    day1

---
+++ flutter_portfolio/lib/screens/homePage.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (
+          context,con
+          ),
+        ),
+      appBar: AppBar(
+        title: Text(
+          "Portfolio",
+          style: TextStyle(
+            fontFamily: 
+          ),
+        ),
+      ),
+    );
+  }
+}
